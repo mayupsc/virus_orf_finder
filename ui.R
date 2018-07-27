f20a048f956a2c4705c90bf8a0bd48e06ff1aab7
mayupsc <37583768+mayupsc@users.noreply.github.com>
2018-07-27 20:42:28 +0800

    Update ui.R

--- ui.R
+++ ui.R
@@ -10,11 +10,6 @@ ui <- dashboardPage(
   dashboardHeader(title = "Virus ORF Alignment",disable = F),
   dashboardSidebar(
     fileInput("file", "input virus genome files(.zip)"),
-  sidebarMenu(
-    menuItem("Work Flow", tabName = "workflow", icon = icon("th")),
-    menuItem("Make Database", tabName = "database", icon = icon("file",lib = "font-awesome"))
-    ),
-  
     textInput("minorflen","Minimal ORF length (nt) for ORF finder:","50"),
     actionButton(class = "btn-sm", "refresh",tagList(icon("refresh"), "Refresh")),
     textInput("evalue","Expectation value (E) threshold for BLAST:","1e-10"),
@@ -26,75 +21,52 @@ ui <- dashboardPage(
         p(class="small"," "),
         p(class = "small","Xing Fu Ph.D. "),
         p(class = "small","Bioinfomatics Core Facility")
-
     )
    ),
   dashboardBody(
-    
-    tabItems(
-      tabItem(tabName = "workflow",
-              h3("Work Flow"),
-              fluidRow(
-              img(src="workflow",height="70%",width="80%"),
-              verbatimTextOutput("preparation")
-              )
-      ),
-      tabItem(tabName = "database",
-              h3("ORF database"),
-              fluidRow(
-                tabBox(width = 12, height = NULL,
+    fluidRow(
+      tabBox(width = 12, height = NULL,
+             tabPanel("workflow",
+                      h3("Work Flow"),
+                      fluidRow(
+                        img(src="workflow",height="70%",width="80%"),
+                        verbatimTextOutput("preparation")
+                        )
+                      ),
+             tabPanel("orfdatabase", 
+                      h3("ORF database"),
+                      fluidRow(
+                        box(width = 5,title = "ORF database:",
+                            dataTableOutput("orfdatabase")
+                            ),
+                        box(width = 7, title = "Phylogenetic Tree",solidHeader = TRUE,
+                            collapsible = TRUE, plotOutput("plot1",height = "880px"))
+                        )
+                      ),
+             tabPanel("blast",
+                      h3("BLAST"),
+                      fluidRow(
+                        box(width = 12, title = "ORF Viewer",solidHeader = TRUE,
+                            collapsible = TRUE, plotOutput("plot2",height = "440px")
+                            ),
+                        box(width = 12,collapsible = T,
+                            dataTableOutput("blastp")
+                            )
+                        )
+                      ),
+             tabPanel("Hits Alignment & ORF Distribution",
+                      fluidRow(
+                        box(downloadButton("downloadHTML","DownloadHTML")),
+                        box(width = 12, title = "ORF Hits Alignment", collapsible = T,solidHeader = TRUE,htmlOutput("msa"))
+                        ),
+                      fluidRow(
+                        box(downloadButton("downloadPDF","DownloadPDF")),
+                        box(width = 12,title = "Phylogenetic Tree",solidHeader = T,collapsible = T,uiOutput("phylo"))
+                        )
+                      )
+             )
+      )
+    )
+  )
 
-                  tabPanel("orfdatabase", 
-                                h3("ORF database"),
-                                # fluidRow(
-                                #   box(width = 8,
-                                #       title = "Tree Identifier Simplifier", 
-                                #       solidHeader = TRUE,
-                                #       textInput("raw_name","full name of virus genus","Tomato yellow leaf curl"),
-                                #       textInput("new_name","short name of virus genus:","TYLC")
-                                #   )
-                                # ),
-                                fluidRow(
-                                  box(
-                                    width = 5,
-                                    title = "ORF database:",
-                                    dataTableOutput("orfdatabase")
-                                  ),
-                                  box( width = 7, title = "Phylogenetic Tree",solidHeader = TRUE,
-                                       collapsible = TRUE, plotOutput("plot1",height = "880px"))
-                                )                                  
-                                 ),
-                       
-                  tabPanel("blast",              
-                        h3("BLAST"),
-                        fluidRow(
-                                  box( width = 12, title = "ORF Viewer",solidHeader = TRUE,
-                                       collapsible = TRUE, plotOutput("plot2",height = "440px")
-                                       ),
-                                  box(width = 12,collapsible = T,
-                                      dataTableOutput("blastp")
-                                       )
-                                )
-                           ),
-                  tabPanel("Hits Alignment & ORF Distribution",
-                           fluidRow(
-                             box(downloadButton("downloadHTML","DownloadHTML")),
-                             box(width = 12, title = "ORF Hits Alignment", collapsible = T,solidHeader = TRUE,htmlOutput("msa"))
-                           ),
-      
-                           fluidRow(
-                                  box(downloadButton("downloadPDF","DownloadPDF")),
-                                  box(width = 12,title = "Phylogenetic Tree",solidHeader = T,collapsible = T,uiOutput("phylo"))
-                                    )
-                           )
-                                
-                                
-                       )
-              )
-                )
-
-
-)
-)
-)
 
