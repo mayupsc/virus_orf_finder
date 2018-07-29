7b4c459ebd753580ad26472c6d1d9ad20bbde27f
mayupsc <37583768+mayupsc@users.noreply.github.com>
2018-07-29 13:07:43 +0800

    Update ui.R

--- ui.R
+++ ui.R
@@ -1,9 +1,3 @@
-library(shiny)
-library(data.table)
-library(jsonlite)
-library(shinydashboard)
-library(seqinr)
-library(DECIPHER)
 ui <- dashboardPage(
 
 
@@ -11,7 +5,7 @@ ui <- dashboardPage(
   dashboardSidebar(
     fileInput("file", "input virus genome files(.zip)"),
     textInput("minorflen","Minimal ORF length (nt) for ORF finder:","50"),
-    actionButton(class = "btn-sm", "refresh",tagList(icon("refresh"), "Refresh")),
+    #actionButton(class = "btn-sm", "refresh",tagList(icon("refresh"), "Refresh")),
     textInput("evalue","Expectation value (E) threshold for BLAST:","1e-10"),
     textInput("raw_name","full name of virus genus","Tomato yellow leaf curl"),
     textInput("new_name","short name of virus genus:","TYLC"),
@@ -29,19 +23,24 @@ ui <- dashboardPage(
              tabPanel("workflow",
                       h3("Work Flow"),
                       fluidRow(
-                        img(src="workflow",height="70%",width="80%"),
+                        img(src="workflow",height="70%",width="60%"),
                         verbatimTextOutput("preparation")
                         )
                       ),
              tabPanel("orfdatabase", 
                       h3("ORF database"),
+                      # fluidRow(
+                      #   box(width = 5,title = "choose interest virus:",
+                      #       uiOutput("species")
+                      # ),
                       fluidRow(
                         box(width = 5,title = "ORF database:",
-                            dataTableOutput("orfdatabase")
+                            DT::dataTableOutput("orfdatabase")
                             ),
                         box(width = 7, title = "Phylogenetic Tree",solidHeader = TRUE,
                             collapsible = TRUE, plotOutput("plot1",height = "880px"))
                         )
+                     # )
                       ),
              tabPanel("blast",
                       h3("BLAST"),
@@ -50,7 +49,7 @@ ui <- dashboardPage(
                             collapsible = TRUE, plotOutput("plot2",height = "440px")
                             ),
                         box(width = 12,collapsible = T,
-                            dataTableOutput("blastp")
+                            DT::dataTableOutput("blastp")
                             )
                         )
                       ),
@@ -60,13 +59,18 @@ ui <- dashboardPage(
                         box(width = 12, title = "ORF Hits Alignment", collapsible = T,solidHeader = TRUE,htmlOutput("msa"))
                         ),
                       fluidRow(
+                        h4("Phylo Tree"),
                         box(downloadButton("downloadPDF","DownloadPDF")),
                         box(width = 12,title = "Phylogenetic Tree",solidHeader = T,collapsible = T,uiOutput("phylo"))
                         )
                       )
              )
-      )
+      
     )
   )
+)
+
+
+
 
 
